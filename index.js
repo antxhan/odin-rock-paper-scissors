278a2f8015f4832b769d754d0020f877c61f7b02
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-08-31 11:56:18 +0200

    feat: multiple rounds

--- index.js
+++ index.js
@@ -30,14 +30,17 @@ function getHumanChoice() {
   return choice;
 }
 
-function playRound(computerChoice, humanChoice) {
+function playRound(round) {
+  console.log(`================${round}================`);
+  const computerChoice = getComputerChoice();
+  const humanChoice = getHumanChoice();
   console.log("computer:", computerChoice);
   console.log("human:", humanChoice);
   if (choices[computerChoice].beats === humanChoice) {
-    console.log("human loses");
+    console.log("computer wins this round");
     computerScore++;
   } else if (choices[computerChoice].beatenBy === humanChoice) {
-    console.log("human wins");
+    console.log("human wins this round");
     humanScore++;
   } else if (computerChoice === humanChoice) {
     console.log("tie");
@@ -46,16 +49,26 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
-playRound(computerChoice, humanChoice);
+function playGame() {
+  const maxRounds = 5;
+  for (let round = 0; round < maxRounds; round++) {
+    playRound(round + 1);
+  }
+  console.log("================RESULTS================");
+  console.log(
+    "computer score:",
+    computerScore,
+    "   |   ",
+    "human score: ",
+    humanScore
+  );
+  if (computerScore > humanScore) {
+    console.log("computer wins");
+  } else if (computerScore < humanScore) {
+    console.log("human wins");
+  } else {
+    console.log("tie");
+  }
+}
 
-// for (let i = 0; i < choices.length; i++) {
-//   for (let j = 0; j < choices.length; j++) {
-//     const computerChoice = choices[i];
-//     const humanChoice = choices[j];
-//     console.log(computerChoice);
-//     console.log(humanChoice);
-//     playRound(computerChoice, humanChoice);
-//   }
-// }
+playGame();
